3bc53af04bd3fa0eab6e8fcea60ff74476295869
GoncharenkoVladimir <kragen1988@gmail.com>
2016-07-31 09:05:26 +0300

    Added tabs clicker

--- frontend/js/tabs.js
+++ frontend/js/tabs.js
@@ -1,4 +1,12 @@
-function tabsInit(tabs){
-
+function tabsInit(tab){
+    var tabs = $(tab);
+    tabs.find('.tab').on('click', function(){
+        $('.tab').removeClass('active');
+        $(this).addClass('active');
+        var panel = $(this).attr('tab');
+        $('.panel').removeClass('active');
+        $('.panel[name='+ panel+']').attr('name', panel).addClass('active');
+    })
 }
+
 tabsInit("#tabs");
